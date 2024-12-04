0d4a858f2af2987bb5141db2141c55378aeacd32
Mahmoud Sidky <ma7mod_a@live.com>
2024-12-04 16:13:39 +0300

    first commit, initailized the project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'outer-space': '#1d2424',
+        'sundance' : '#ccab60',
+        'oslo-gray' : '#8c9494',
+        'manatee': '#8c8c94'
+
+      },
+    },
+  },
+  plugins: [],
+}
+
